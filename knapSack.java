f43e9e53875709096c96ff90d766b616861a9b0a
Rajesh kumar jena <129972543+Rajesh-1234567@users.noreply.github.com>
2023-11-14 00:27:51 +0530

    Create knapSack.java

---
+++ knapSack.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class knapSack {
+    public static void main(String []args){
+     int []weight={10,20,30};
+     int []value= {60,100,120};
+     int capacity=50;
+     int ans=0;
+     double [][]table=new double[weight.length][2];  
+     for(int i=0;i<weight.length;i++){
+        table[i][0]=i;
+        table[i][1]=value[i]/(double)weight[i];
+     }
+        Arrays.sort(table, Comparator.comparingDouble(o->o[1]));
+        for(int i=weight.length-1;i>=0;i--){
+            int idx=(int) table[i][0];
+            if(capacity >=weight[idx]){
+                ans += value[idx];
+                capacity -= weight[idx];
+            }else{
+                ans += ( table[i][1]*capacity); 
+                capacity=0;break;
+            }
+        }
+        System.out.println("The maxm value is : "+ ans);
+     
+    }
+}
